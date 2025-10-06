1572221d9d0e9c62e9316bb37f2ea1cd493745cf
Angela Gutierrez <an.gutierrez99@gmail.com>
2025-10-06 17:09:54 +0200

    Add excercise_python_0.py

---
+++ excercise_python_0.py
@@ -0,0 +1,4 @@
+import sys
+import os
+file_path = sys.argv[1]
+print(os.basename(file_path))
---
+++ input_arguments_test.py
@@ -0,0 +1,7 @@
+import sys
+
+# use the first and only argument from the user and save it as "input_string"
+input_string = sys.argv[1]
+
+# print the input argument
+print(input_string)
